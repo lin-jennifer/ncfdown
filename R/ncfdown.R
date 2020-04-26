322a4db63669ba9cbc7d6de7714a0743d1a23c6c
Jennifer Lin <jennifer.lin16@ncf.edu>
2020-04-26 13:02:37 -0400

    NCF RMD

---
+++ R/ncfdown.R
@@ -0,0 +1,24 @@
+library(rmarkdown)
+
+# Features of NCF Template
+ncf <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
+  
+  # get the locations of resource files located within the package
+  css <- system.file("rmarkdown", "templates", "ncf" ,"resources", "style.css", package = "ncfdown")
+  template <- system.file("rmarkdown", "templates", "ncf" ,"resources", "template_ncf.html", package = "ncfdown")
+  
+  # call the base html_document function
+  rmarkdown::html_document( theme= "lumen",
+                            template= template,
+                            css= css,
+                            toc= toc,
+                            toc_float = TRUE,
+                            toc_depth = 2,
+                            number_sections= number_sections,
+                            df_print = "paged",
+                            code_folding = code_folding,
+  )
+}
+
+
+
