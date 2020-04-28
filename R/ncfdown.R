3bd3ed8684bf8e952345b1b9414d19513a1528af
Jennifer Lin <jennifer.lin16@ncf.edu>
2020-04-27 20:18:02 -0400

    NCFWEB dependency

--- R/ncfdown.R
+++ R/ncfdown.R
@@ -21,4 +21,22 @@ ncf <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
 }
 
 
+# Features of NCF Template
+ncfweb <- function(toc = TRUE, number_sections=TRUE) {
+
+  # get the locations of resource files located within the package
+  css <- system.file("rmarkdown", "templates", "ncfweb" ,"resources", "style.css", package = "ncfdown")
+  template <- system.file("rmarkdown", "templates", "ncfweb" ,"resources", "template_ncfweb.html", package = "ncfdown")
+
+  # call the base html_document function
+  rmarkdown::html_document( theme= "readable",
+                            template= template,
+                            css= css,
+                            toc= toc,
+                            toc_float = TRUE,
+                            toc_depth = 2,
+                            number_sections= number_sections,
+                            df_print = "paged",
+  )
+}
 
